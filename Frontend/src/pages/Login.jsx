5a8a3ffcc04cc16c5941288541c5d3ee56c8a34f
Diksha <gautamdikshya55@gmail.com>
2025-12-07 23:37:30 +0545

    Updated README with full project details

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -60,7 +60,7 @@ export default function Login() {
         <p className="mt-4 text-center text-sm">
           Don’t have an account?{" "}
           <a href="/register" className="text-blue-600 underline">
-            Register
+            
           </a>
         </p>
       </form>
